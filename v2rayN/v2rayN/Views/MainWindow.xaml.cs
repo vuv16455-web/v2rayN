53cd395ef4a643d5303367a8bf5d364f930f91ba
vuv16455-web <vuv16455@gmail.com>
2026-07-30 11:51:48 +0800

    Update MainWindow.xaml.cs

--- v2rayN/v2rayN/Views/MainWindow.xaml.cs
+++ v2rayN/v2rayN/Views/MainWindow.xaml.cs
@@ -243,10 +243,6 @@ public partial class MainWindow
         ShowHideWindow(false);
     }
 
-    private void MenuPromotion_Click(object sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
-    }
 
     private void MenuSettingsSetUWP_Click(object sender, RoutedEventArgs e)
     {
